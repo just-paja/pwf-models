da7a0b2e6923f2ed0d998ada0b53fe6c4dab2230
just-paja <just.paja@gmail.com>
2013-12-27 18:53:19 +0100

    Sanity test draft

---
+++ test/00-sanity.test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+pwf = require('../node_modules/pwf-comm/node_modules/pwf-config/node_modules/pwf.js');
+$ = require('../node_modules/pwf-jquery-compat/node_modules/jquery');
+require('pwf-jquery-compat');
+require('../node_modules/pwf-comm/node_modules/pwf-config');
+require('../node_modules/pwf-comm/node_modules/pwf-storage');
+require('../node_modules/pwf-comm/node_modules/pwf-queue');
+require('pwf-comm');
+
+
+describe('sanity', function() {
+	it('tests only sanity of js code', function() {
+		assert.doesNotThrow(function() {
+			mod = require('../lib/model');
+		}, 'An error was thrown during mod inclusion of model lib.');
+
+		assert.doesNotThrow(function() {
+			mod = require('../lib/list');
+		}, 'An error was thrown during mod inclusion of list lib.');
+	});
+});
